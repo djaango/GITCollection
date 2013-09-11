35c2195e077b8e0dee626c3213a1ad3b7a26af56
djaango <marko.jaankovic@gmail.com>
2013-09-11 18:19:51 +0200

    erster try

---
+++ CollectionTester.java
@@ -0,0 +1,30 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: marko
+ * Date: 05.08.13
+ * Time: 19:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class CollectionTester {
+
+    public static CollectionTester instance = null;
+
+    public CollectionTester(){
+        instance=this;
+    }
+
+    public static void main(String [] args){
+        int hallo = 2;
+        CollectionTester coltest = new CollectionTester();
+
+    }
+
+    public void giveInstanceTo(){
+
+    }
+
+    public static CollectionTester getInstance(){
+        if(instance==null) return null;
+        else return instance;
+    }
+}

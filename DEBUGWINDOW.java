5c276825ba2ea7393be31534548d4eae12e435be
djaango <marko.jaankovic@gmail.com>
2013-09-11 21:40:21 +0200

    erster try

--- DEBUGWINDOW.java
+++ DEBUGWINDOW.java
@@ -16,7 +16,7 @@ public class DEBUGWINDOW extends JFrame implements ActionListener {
     public Button but = null;
     public TextArea ta = new TextArea(20,50);
     public static DEBUGWINDOW instance = null;
-    public int stella = 0;
+    public int heinrich = 11;
     // Ende Attribute
 
 
